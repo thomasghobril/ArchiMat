69a84863835d905e92cfe1471aafe9f626246647
Laurent Cabaret <laurent.cabaret@gmail.com>
2020-09-28 18:17:45 +0200

    Création du code Cache froid

---
+++ 02-OpenMP-CF/src/main.cpp
@@ -0,0 +1,100 @@
+#include <chrono>
+#include <random>
+#include <iostream>
+#include <float.h>
+#include <omp.h>
+
+
+double sequentiel(double* A, double* B, double* S, unsigned long int size) {
+    for (unsigned long int i = 0; i < size; i++) {
+        S[i] = (A[i] + B[i])/2;
+    }
+    return S[4];
+}
+
+double parallele(double* A,double* B, double*S, unsigned long int size) {
+    #pragma omp parallel for schedule(static)
+        for (unsigned long int i = 0; i < size; i++) {
+            S[i] = (A[i] + B[i])/2;
+        }
+    return S[4];
+}
+
+int main() {
+    
+    /* initialize random seed: */
+    srand (time(NULL));
+
+    for(unsigned long long size = 1024; size<(128*1024*1024);size*=1.2) {
+        //unsigned long long iter = 10*(128*1024*1024/size);
+        unsigned long long iter = 20;
+
+    // Création des données de travail
+    double * A,* B,* C,* S1,* S2, * A1,* B1,* A2,* B2;
+    A1 = (double *) malloc(size * sizeof(double));
+    B1 = (double *) malloc(size * sizeof(double));
+    A2 = (double *) malloc(size * sizeof(double));
+    B2 = (double *) malloc(size * sizeof(double));
+
+    S1 = (double *) malloc(size * sizeof(double)); // Résultat Séquentiel
+    S2 = (double *) malloc(size * sizeof(double)); // Résultat Parallèle
+
+
+    for (unsigned long int i = 0; i < size; i++) {
+        A1[i] = (double)(rand() % 360 - 180.0);
+        B1[i] = (double)(rand() % 360 - 180.0);
+        A2[i] = (double)(rand() % 360 - 180.0);
+        B2[i] = (double)(rand() % 360 - 180.0);
+
+    }
+
+   
+    auto result = 0.0f; //utilisé pour s'assurer que le compilateur nu supprime pas les données
+    std::chrono::high_resolution_clock::time_point t0;
+    std::chrono::high_resolution_clock::time_point t1;
+    double min_duration = DBL_MAX;
+    
+    t0 = std::chrono::high_resolution_clock::now();
+    for (auto it =0; it < iter; it++) {
+        if (it%2==0) {A=A1;B=B1;} else {A=A2;B=B2;}
+        result += sequentiel(A, B, S1, size);
+    }
+    t1 = std::chrono::high_resolution_clock::now();
+    double seq_duration = std::chrono::duration<double>(t1-t0).count();
+    seq_duration /= (size*iter);
+
+    t0 = std::chrono::high_resolution_clock::now();
+    for (auto it =0; it < iter; it++) {
+        if (it%2==0) {A=A1;B=B1;} else {A=A2;B=B2;}
+        result += parallele(A, B, S2, size);
+    }
+    t1 = std::chrono::high_resolution_clock::now();
+    double par_duration = std::chrono::duration<double>(t1-t0).count();
+    par_duration /= (size*iter);
+    
+    std::cout << 8*size/1024 << " " << seq_duration << " " << par_duration << " " << seq_duration/par_duration << " " << iter << " " << result << " " << std::endl;
+    // std::cout << size << " " << seq_duration << " " << par_duration << std::endl;
+
+    /*** Validation ***/
+    bool valide = false;
+    for (unsigned long int i = 0; i < size; i++) {
+        if(S1[i] == S2[i]) {
+            valide = true;
+        }
+        else {
+            valide = false;
+            break;
+        }
+    }
+
+    // Libération de la mémoire : indispensable
+
+    free(A1);
+    free(B1);
+    free(A2);
+    free(B2);
+    free(S1);
+    free(S2);    
+}
+    return 0;
+}
