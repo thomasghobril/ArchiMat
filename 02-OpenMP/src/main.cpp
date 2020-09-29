b8e5dc7e835fedb1600126a243193496a6e409a1
Laurent Cabaret <laurent.cabaret@gmail.com>
2020-09-29 07:55:34 +0200

    cleanCache upgrade

--- 02-OpenMP/src/main.cpp
+++ 02-OpenMP/src/main.cpp
@@ -20,6 +20,13 @@ double parallele(double* A, double* B, double*S, unsigned long int size) {
     return S[4];
 }
 
+double clean(double* cleanCache, unsigned long int maxSize) {
+    #pragma omp parallel for schedule(static)
+        for (unsigned long int i = 0; i < maxSize; i++) {
+            cleanCache[i] = (cleanCache[i] + cleanCache[i])/2;
+        }
+    return cleanCache[4];
+}
 int main(int argc, char* argv[]) {
     
     /* initialize random seed: */
@@ -27,18 +34,21 @@ int main(int argc, char* argv[]) {
     unsigned short int cores = atoi(argv[1]);
     
     int maxCores = omp_get_max_threads();
+    int maxSize = (16*1024*1024)/sizeof(double);
     std::cout << " Moyenne avec cache chaud " << cores <<":"<< maxCores << std::endl;
    
    for (auto cores =2; cores <= maxCores; cores+=2) {
        omp_set_num_threads(cores);
 
-    for(unsigned long long size = 8192/8; size<(32*1024*1024)/8;size = size + std::max(1024, int(size*0.1))) {
+    for(unsigned long long size = 8192/sizeof(double); size<(32*1024*1024)/sizeof(double);size = size + std::max(1024, int(size*0.1))) {
         unsigned long long iter = 45;
 
     // Création des données de travail
     double * A,* B,* C,* S1,* S2;
     A = (double *) malloc(size * sizeof(double));
     B = (double *) malloc(size * sizeof(double));
+
+    double * cleanCache = (double *) malloc(maxSize * sizeof(double));
 
     S1 = (double *) malloc(size * sizeof(double)); // Résultat Séquentiel
     S2 = (double *) malloc(size * sizeof(double)); // Résultat Parallèle
@@ -48,7 +58,9 @@ int main(int argc, char* argv[]) {
         A[i] = (double)(rand() % 360 - 180.0);
         B[i] = (double)(rand() % 360 - 180.0);
     }
-
+    for (unsigned long int i = 0; i < maxSize; i++) {
+        cleanCache[i] = (double)(rand() % 360 - 180.0);
+    }
    
     auto result = 0.0f; //utilisé pour s'assurer que le compilateur nu supprime pas les données
     std::chrono::high_resolution_clock::time_point t0;
@@ -59,6 +71,7 @@ int main(int argc, char* argv[]) {
     t0 = std::chrono::high_resolution_clock::now();
     for (auto it =0; it < iter; it++) {
         result += sequentiel(A, B, S1, size);
+        result += clean(cleanCache, maxSize);
     }
     t1 = std::chrono::high_resolution_clock::now();
     double seq_duration = std::chrono::duration<double>(t1-t0).count();
@@ -68,6 +81,7 @@ int main(int argc, char* argv[]) {
         t0 = std::chrono::high_resolution_clock::now();
         for (auto it =0; it < iter; it++) {
             result += parallele(A, B, S2, size);
+            result += clean(cleanCache, maxSize);
         }
         t1 = std::chrono::high_resolution_clock::now();
         double par_duration = std::chrono::duration<double>(t1-t0).count();
