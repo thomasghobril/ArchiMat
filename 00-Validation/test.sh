ce964abf1bacac0bca89b8ba7f222c3c279fb471
Laurent CABARET <laurent.cabaret@centralesupelec.fr>
2023-09-26 22:31:12 +0200

    test.sh adaptation

--- 00-Validation/test.sh
+++ 00-Validation/test.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-make clean; make
 
-for I in {32..4096..8}
+set -o errexit # stop en cas d'erreur
+
+make clean; make # regénérer les executables
+
+NB_ITER=10
+
+for I in {32..4096..8} 
 do
-(( ITER=10))
-exe/test.exe $I $I $ITER
+echo $I
+exe/test.exe $I $I $NB_ITER
 done
-
-
